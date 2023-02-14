0ee8dc7f08ef090bb1f683442693898159af3313
Victor Delgado <valexdelgado@gmail.com>
2023-02-13 19:34:03 -0800

    forgot to push code from earlier today at work. switching plan to: plan out routes and pages/placeholders

--- capstone_v1/static/scripts.js
+++ capstone_v1/static/scripts.js
@@ -2,7 +2,7 @@ const API_KEY = 9973533
 const QUERY = 'chicken'
 
 async function getSomeData(){
-        const someData = await axios.get(`https://www.themealdb.com/api/json/v2/${API_KEY}/search.php?s=${QUERY}`)
+        const someData = await axios.get(`https://www.themealdb.com/api/json/v2/${API_KEY}/search.php?`, {params: {s:QUERY})
         console.log(someData.data)
     }
 getSomeData()
