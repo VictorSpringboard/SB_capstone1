cdbab1cd9d986c2be597d581621c150fb3aed106
Victor Delgado <valexdelgado@gmail.com>
2023-02-14 10:09:06 -0800

    current functionality: can enter search term (by name), UL appears with meal names

--- capstone_v1/static/scripts.js
+++ capstone_v1/static/scripts.js
@@ -1,8 +1,21 @@
 const API_KEY = 9973533
-const QUERY = 'chicken'
+const QUERY = document.getElementById('searchBox')
+
+const theList = document.querySelector('.stuff')
 
 async function getSomeData(){
-        const someData = await axios.get(`https://www.themealdb.com/api/json/v2/${API_KEY}/search.php?s=${QUERY}`)
-        console.log(someData.data)
-    }
-getSomeData()
+        const someData = await axios.get(`https://www.themealdb.com/api/json/v2/${API_KEY}/search.php?`, {params:{s:QUERY.value}})
+        // console.log(someData.data.meals)
+
+        for (let meal of someData.data.meals){
+            console.log(meal.strMeal)
+            const item = document.createElement('li')
+            item.innerHTML = meal.strMeal
+            theList.appendChild(item)
+        }
+}
+
+const searchButton = document.querySelector('#searchForm').addEventListener('submit', (e)=>{
+    e.preventDefault()
+    getSomeData()
+})
