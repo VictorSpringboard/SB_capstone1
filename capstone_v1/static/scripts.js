f7a47ef93cb1bba623b668fee2779070b8e9f717
Victor Delgado <valexdelgado@gmail.com>
2023-02-14 10:57:44 -0800

    working on adding display cards for recipes found from search queries

--- capstone_v1/static/scripts.js
+++ capstone_v1/static/scripts.js
@@ -3,19 +3,35 @@ const QUERY = document.getElementById('searchBox')
 
 const theList = document.querySelector('.stuff')
 
+const mealIdList = []
 async function getSomeData(){
-        const someData = await axios.get(`https://www.themealdb.com/api/json/v2/${API_KEY}/search.php?`, {params:{s:QUERY.value}})
-        // console.log(someData.data.meals)
+        const someData = await axios.get(`https://www.themealdb.com/api/json/v2/${API_KEY}/filter.php?`, {params:{i:QUERY.value}})
+        console.log(someData.data)
 
         for (let meal of someData.data.meals){
-            console.log(meal.strMeal)
-            const item = document.createElement('li')
-            item.innerHTML = meal.strMeal
-            theList.appendChild(item)
+            mealIdList.push(meal.idMeal)
         }
+        console.log('The Meal id List is')
+        console.log(mealIdList.length)
+        console.log(mealIdList)
+
+
+        // This is where the cards for each meal get made (I'll use bootstrap cards for each meal)
+
+
+        // bootstrap card etc etc etc
+        
 }
+
 
 const searchButton = document.querySelector('#searchForm').addEventListener('submit', (e)=>{
     e.preventDefault()
     getSomeData()
 })
+ 
+async function getMealData(id){
+    const someData = await axios.get(`https://www.themealdb.com/api/json/v2/${API_KEY}/lookup.php?`, {params:{i:id}})
+        console.log(someData.data)
+}
+
+getMealData(52772)
