de0881ba31ce64677a3ae99144ec6d310226fea2
Victor Delgado <valexdelgado@gmail.com>
2023-03-05 12:24:42 -0800

    fixed some unmerged merges

--- models.py
+++ models.py
@@ -25,15 +25,6 @@ class Favorite(db.Model):
     category = db.Column(db.Text, nullable=False)
     area = db.Column(db.Text, nullable=False)
     original = db.Column(db.Text, nullable=False)
-<<<<<<< HEAD
-
-class Grocery(db.Model):
-    __tablename__ = 'groceries'
-    
-    user_id = db.Column(db.Integer, db.ForeignKey('users.id', ondelete='cascade'), primary_key=True)
-    ingredient_id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.Text, nullable=False)
-=======
     
     def getTitles(self):
         return {'title': self.title}
@@ -85,47 +76,11 @@ class Message(db.Model):
     body = db.Column(db.Text)
     timestamp = db.Column(db.DateTime, index=True, default = datetime.datetime.utcnow)
 
->>>>>>> messages
 
     
 class User(db.Model):
     __tablename__ = 'users'
     
-<<<<<<< HEAD
-    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
-    username = db.Column(db.String(20), unique=True)
-    password = db.Column(db.Text, nullable=False)
-    email = db.Column(db.String(50), nullable=False)
-    
-    groceries = db.relationship('User', secondary='groceries', primaryjoin=(Grocery.user_id == id))
-    
-    favorites = db.relationship('User', secondary='favorites', primaryjoin=(Favorite.user_id == id))
-
-    def __repr__(self):
-        return f'User: {self.username}, email: {self.email}'
-    
-    @classmethod
-    def register_user(cls, id, username, pwd, email):
-        
-        hashed = bcrypt.generate_password_hash(pwd)
-        
-        hashed_utf8 = hashed.decode('utf8')
-        
-        return cls(id=id,
-                    username=username, 
-                    password=hashed_utf8,
-                    email=email)
-    
-    @classmethod
-    def authenticate_user(cls, username, pwd):
-        
-        u = User.query.filter_by(username=username).first()
-        
-        if u and bcrypt.check_password_hash(u.password, pwd):
-            return u
-        else:
-            return False
-=======
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(20), unique=True)
     password = db.Column(db.Text, nullable=False)
@@ -175,23 +130,7 @@ class User(db.Model):
             if is_auth:
                 return u
         return False
->>>>>>> messages
         
     
     
         
-<<<<<<< HEAD
-class Recipe(db.Model):
-    __tablename__ = 'recipes' 
-    
-    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
-    title = db.Column(db.Text, nullable=False)
-    ingredients = db.Column(db.Text, nullable=False)
-    measurements = db.Column(db.Text, nullable=False)
-    instructions = db.Column(db.Text, nullable=False)
-    category = db.Column(db.Text, nullable=False)
-    area = db.Column(db.Text, nullable=False)
-    original = db.Column(db.Text, nullable=False)
-    
-=======
->>>>>>> messages
