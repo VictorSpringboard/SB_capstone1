fdac722e3bb4fa653fbe5d706f2c4c629cdf4a0d
Victor Delgado <valexdelgado@gmail.com>
2023-03-08 17:52:47 -0800

    can now delete messages

--- app.py
+++ app.py
@@ -24,10 +24,10 @@ API_KEY = 9973533
 app = Flask(__name__)
 bootstrap = Bootstrap()
 # home db
-# app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL', 'postgresql://postgres:admin@localhost/yumble')
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL', 'postgresql://postgres:admin@localhost/yumble')
 
 # work db
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///yumble.db'
+# app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///yumble.db'
 
 # test db
 app.config['SQLALCHEMY_BINDS'] = {'testDB': 'sqlite:///test_yumble.db'}
@@ -301,3 +301,13 @@ def send_message(recipient):
         db.session.commit()
         return redirect(f'/users/{g.user.id}/profile')
     return render_template('send_message.html', form=form, recipient=user)
+
+@app.route('/users/<int:message_id>/delete_message', methods=['GET', 'POST'])
+def delete_message(message_id):
+    
+    user_id = g.user.id
+    msg = Message.query.get(message_id)
+    db.session.delete(msg)
+    db.session.commit()
+    
+    return redirect(f'/users/{user_id}/messages')
